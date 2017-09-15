404acfa1ad25eb9c8d011313ce3a93900b419d5f
Aashis <g2aashis@gmail.com>
2017-09-15 00:53:11 -0400

    Load and render image

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import scipy.io as spio
+from PIL import Image
+
 import app_config as cfg
-import preprocess.load.av_wide_load as loader
-import os
 
 if __name__ == '__main__':
     file = cfg.path(cfg.av_wide_data, 'wide_image_01.mat')
-    loader.load_matlab(file)
+    img = spio.loadmat(file)
+    I2 = img['I2']
+    G_struct = img['G']
+    A = G_struct['A'][0]
+    print(A.shape)
+    # Image.fromarray(I2[:, :, 1], mode='L').show()
+    # Image.fromarray(A).show()
