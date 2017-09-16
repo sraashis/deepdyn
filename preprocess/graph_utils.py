be5341156dd4d1b54f5f6e5751222827f54236da
Aashis <g2aashis@gmail.com>
2017-09-15 20:59:35 -0400

    Edge color black set

--- preprocess/graph_utils.py
+++ preprocess/graph_utils.py
@@ -4,7 +4,7 @@ import networkx as nx
 
 def show_graph(auxiliary_matrix, node_pos=None, node_color='r'):
     graph = nx.from_scipy_sparse_matrix(auxiliary_matrix)
-    nx.draw_networkx(graph, pos=node_pos, edge_color='b', node_color=node_color, with_labels=False, node_size=2.5,
+    nx.draw_networkx(graph, pos=node_pos, edge_color='black', node_color=node_color, with_labels=False, node_size=2.5,
                      width=0.5)
     plt.show()
 
