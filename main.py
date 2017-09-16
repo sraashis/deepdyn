2ab25d6b496d9e40153e24b6b719a25e3a56bf7b
Aashis <g2aashis@gmail.com>
2017-09-15 20:17:29 -0400

    Added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from mat_utils import Mat
-import graph_utils as gt
-import numpy as np
-import image_utils as img
+from preprocess.mat_utils import Mat
+from preprocess import image_utils as img
+
+from preprocess import graph_utils as gt
 
 if __name__ == '__main__':
 
@@ -9,15 +9,15 @@ if __name__ == '__main__':
 
     image = file.get_image('I2')
 
-    # img.show_image(image[:,:,1])
-    # img.slide_and_construct(image[:, :, 1])
+    img.slide_and_construct(image[:, :, 1])
+    img.show_image(image[:, :, 1])
 
-    auxiliary_graph = file.get_graph('A')
-    node_pos = file.get_graph('V')
-
-    veins_color = (gt.color_vein(v) for v in file.get_graph('art'))
-    arteries_color = (gt.color_artery(a) for a in file.get_graph('ven'))
-
-    color = (gt.color_av(a, v) for a, v in zip(file.get_graph('art'), file.get_graph('ven')))
-
-    gt.show_graph(auxiliary_matrix=auxiliary_graph,node_pos=node_pos,node_color=''.join(color))
+    # auxiliary_graph = file.get_graph('A')
+    # node_pos = file.get_graph('V')
+    #
+    # veins_color = (gt.color_vein(v) for v in file.get_graph('art'))
+    # arteries_color = (gt.color_artery(a) for a in file.get_graph('ven'))
+    #
+    # color = (gt.color_av(a, v) for a, v in zip(file.get_graph('art'), file.get_graph('ven')))
+    #
+    # gt.show_graph(auxiliary_matrix=auxiliary_graph,node_pos=node_pos,node_color=''.join(color))
--- preprocess/image_utils.py
+++ preprocess/image_utils.py
@@ -8,7 +8,7 @@ import app_config as cfg
 
 # @args(2d_array, m, n)
 # # 50 * 50 Sliding window by default
-def slide_and_construct(nd_array, m=50, n=50, threshold=0.95):
+def slide_and_construct(nd_array, m=50, n=50, threshold=0.90):
     x = nd_array.shape[0]
     y = nd_array.shape[1]
     x_w = m - 1
@@ -21,11 +21,11 @@ def slide_and_construct(nd_array, m=50, n=50, threshold=0.95):
                 window_x = 0
             if window_y < 0:
                 window_y = 0
-            temp_arr = nd_array[window_x:i + x_w, window_y:j + y_w]
-            temp_x, temp_y = temp_arr.shape[0], temp_arr.shape[1]
-            avg = np.ndarray.sum(temp_arr) / (temp_x * temp_y)
-            mx = np.ndarray.max(temp_arr)
-            mn = np.ndarray.min(temp_arr)
+            window_arr = nd_array[window_x:i + x_w, window_y:j + y_w]
+            temp_x, temp_y = window_arr.shape[0], window_arr.shape[1]
+            avg = np.ndarray.sum(window_arr) / (temp_x * temp_y)
+            mx = np.ndarray.max(window_arr)
+            mn = np.ndarray.min(window_arr)
 
             if i - 1 >= 0:
                 if nd_array[i - 1, j] >= avg * threshold:
