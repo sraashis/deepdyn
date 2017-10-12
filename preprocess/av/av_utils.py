251619fcef21010b2de0cc007dccc5c08928a477
Aashis <g2aashis@gmail.com>
2017-10-11 22:05:07 -0400

    Enhance parameter added.

--- preprocess/av/av_utils.py
+++ preprocess/av/av_utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 from PIL import Image
 from matplotlib import pyplot as plt
+import IPython
 
 import preprocess.image_utils as img
 
@@ -34,11 +35,12 @@ def show_av_graph(av_data_set, image_show=False, onh_show=False, av_show=True):
         plt.scatter(av_ven[:, 0], av_ven[:, 1], color='blue', s=1.0)
 
     if onh_show:
-        plt.plot(onh[:, 0], onh[:, 1], color='yellow')
+        plt.plot(onh[:, 0], onh[:, 1], color='green')
 
     if image_show:
         image_array = av_data_set.get_image('I2')
-        image = Image.fromarray(image_array)
-        enhanced_image = img.enhance(image)
+        image = Image.fromarray(image_array[:, :, :])
+        enhanced_image = img.enhance(image, color=0, contrast=5, brightness=.81, sharpness=2)
+        IPython.display.Image(data=image)
         plt.imshow(enhanced_image, interpolation='nearest', aspect='auto')
         plt.show()
