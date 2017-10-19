386fc9ed3e7cffeab495a6395949fbdbc40c6df6
Aashis <g2aashis@gmail.com>
2017-10-18 21:50:00 -0400

    Gabor filter testing successful.

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-import matplotlib.pyplot as plt
-
+import preprocess.av.av_utils as av
 import preprocess.filter.image_filters as fil
-import preprocess.graph.graph_utils as gt
-from preprocess.common.mat_utils import Mat
 import preprocess.image.image_utils as img
+from preprocess.common.mat_utils import Mat
 
 if __name__ == '__main__':
     file = Mat(file_name='wide_image_03.mat')
 
     image = file.get_image('I2')
 
-    kernels = fil.build_filter_bank(k_size=24, lambd=5.01, sigma=1.79, psi=0.0, gamma=.89)
-    final_image = fil.process(image[:, :, 1], kernels)
-    img.show_image(final_image)
-    # av.show_av_graph(file)
+    # kernels = fil.build_filter_bank(k_size=24, lambd=5.01, sigma=1.79, psi=0.0, gamma=.89)
+    # final_image = fil.process(image[:, :, 1], kernels)
+    # img.show_image(final_image)
+    av.show_av_graph(file, av_only=False,image_show=False)
     # gt.show_vessel_graph(file)
     # plt.show()
--- preprocess/av/av_utils.py
+++ preprocess/av/av_utils.py
@@ -1,7 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-import preprocess.filter.image_filters as fil
 import preprocess.image.image_utils as img
 
 
@@ -9,41 +8,37 @@ def get_onh_radius(av_data_set):
     return np.linalg.norm(av_data_set.get_graph('onh')[0] - av_data_set.get_graph('onh_pos'))
 
 
-def get_av_nodes(av_data_set=None, vessel=""):
-    node_position = av_data_set.get_graph('V')
-    if vessel.lower() == "art":
-        node_marker = av_data_set.get_graph('art')
-    if vessel.lower() == "ven":
-        node_marker = av_data_set.get_graph('ven')
-    av_radius = 2.5 * get_onh_radius(av_data_set=av_data_set)
+# Function to get AV region nodes
+# Pass 'art' for arteries && 'ven' for veins.
+def get_av_nodes_positions(av_data_set=None, vessel="", av_only=True):
+    node_marker = av_data_set.get_graph(vessel)
+    av_radius = 2.5 * get_onh_radius(av_data_set)
     xc, yc = av_data_set.get_graph('onh_pos')[0]
-    for ix, node in enumerate(node_position):
+    for ix, node in enumerate(av_data_set.get_graph('V')):
         x, y = node
         if node_marker[ix] != 1:
             continue
-        if ((x - xc) ** 2 + (y - yc) ** 2) < av_radius ** 2:
+        if ((x - xc) ** 2 + (y - yc) ** 2) < av_radius ** 2 or not av_only:
             yield node
 
 
-def show_av_graph(av_data_set, image_show=True, onh_show=True, av_show=True, gabor_filter=False):
+def show_av_graph(av_data_set, image_show=True, onh_show=True, av_only=True):
+
     onh = av_data_set.get_graph('onh')
-    av_art = np.array(list(get_av_nodes(av_data_set, vessel="art")))
-    av_ven = np.array(list(get_av_nodes(av_data_set, vessel="ven")))
+    av_art = np.array(list(get_av_nodes_positions(av_data_set, vessel="art", av_only=av_only)))
+    av_ven = np.array(list(get_av_nodes_positions(av_data_set, vessel="ven", av_only=av_only)))
 
     av_art = np.ceil(av_art)
     av_ven = np.ceil(av_ven)
 
-    if av_show:
-        plt.scatter(av_art[:, 0], av_art[:, 1], color='red', s=1.0)
-        plt.scatter(av_ven[:, 0], av_ven[:, 1], color='blue', s=1.0)
+    plt.scatter(av_art[:, 0], av_art[:, 1], color='red', s=1.0)
+    plt.scatter(av_ven[:, 0], av_ven[:, 1], color='blue', s=1.0)
 
     if onh_show:
-        plt.plot(onh[:, 0], onh[:, 1], color='yellow')
+        plt.plot(onh[:, 0], onh[:, 1], color='green')
 
     if image_show:
         image_array = av_data_set.get_image('I2')
-        if gabor_filter:
-            kernels = fil.build_filter_bank(k_size=24, lambd=5.01, sigma=1.79, psi=0.0, gamma=.89)
-            image_array = fil.process(image_array[:, :, 1], kernels)
         plt.imshow(img.from_array(image_array), aspect='auto')
-        plt.show()
+    plt.show()
+
