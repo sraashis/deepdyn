7f8dce03c72149055e5bfae661c0574dc09b9acd
Aashis <g2aashis@gmail.com>
2017-10-18 22:35:30 -0400

    Gabor filter testing successful.

--- preprocess/filter/image_filters.py
+++ preprocess/filter/image_filters.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2 as cv
 import numpy as np
 
 import preprocess.image.image_utils as img
@@ -9,7 +9,7 @@ def build_filter_bank(k_size=10, sigma=0, lambd=0, gamma=0, psi=0, k_type=cv2.CV
     for theta in np.arange(0, np.pi, np.pi / k_size + 1):
         params = {'ksize': (k_size, k_size), 'sigma': sigma, 'theta': theta, 'lambd': lambd,
                   'gamma': gamma, 'psi': psi, 'ktype': k_type}
-        kern = cv2.getGaborKernel(**params)
+        kern = cv.getGaborKernel(**params)
         kern /= 0.93 * kern.sum()
         filters.append((kern, params))
     return filters
@@ -18,6 +18,6 @@ def build_filter_bank(k_size=10, sigma=0, lambd=0, gamma=0, psi=0, k_type=cv2.CV
 def process(image, filters):
     accumulator = np.zeros_like(image)
     for kern, params in filters:
-        final_image = cv2.filter2D(image, cv2.CV_8UC3, kern)
+        final_image = cv.filter2D(image, cv.CV_8UC3, kern)
         np.maximum(accumulator, final_image, accumulator)
     return accumulator
