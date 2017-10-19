bb994ea0a48d635db8eb44875ab7fd587c16f76f
Aashis <g2aashis@gmail.com>
2017-10-18 21:18:19 -0400

    Gabor filter testing successful.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import preprocess.common.image_utils as img
+import matplotlib.pyplot as plt
+
 import preprocess.filter.image_filters as fil
+import preprocess.graph.graph_utils as gt
 from preprocess.common.mat_utils import Mat
+import preprocess.image.image_utils as img
 
 if __name__ == '__main__':
     file = Mat(file_name='wide_image_03.mat')
@@ -10,21 +13,6 @@ if __name__ == '__main__':
     kernels = fil.build_filter_bank(k_size=24, lambd=5.01, sigma=1.79, psi=0.0, gamma=.89)
     final_image = fil.process(image[:, :, 1], kernels)
     img.show_image(final_image)
-
-
-# --------------- USAGE Examples ----------------
-# https://stackoverflow.com/questions/30071474/opencv-getgaborkernel-parameters-for-filter-bank
-# veins_color = (gt.color_vein(v) for v in file.get_graph('art'))
-# arteries_color = (gt.color_artery(a) for a in file.get_graph('ven'))
-#
-# color = (gt.color_av(a, v) for a, v in zip(file.get_graph('art'), file.get_graph('ven')))
-# onh = file.get_graph('onh')
-#
-# av = np.array(list(av.get_av_nodes(file)))
-#
-# gt.show_graph(auxiliary_matrix=auxiliary_graph, node_pos=node_pos, node_color=''.join(color))
-# plt.scatter(av[:, 0], av[:, 1], color='black')
-# plt.plot(onh[:, 0], onh[:, 1], color='yellow')
-# plt.show()
-# av.show_av_graph(file)
-# av.region_growing(file)
+    # av.show_av_graph(file)
+    # gt.show_vessel_graph(file)
+    # plt.show()
