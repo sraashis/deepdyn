1a42293fcf0340560cd8ee9a08f770df8fc5737a
Aashis <g2aashis@gmail.com>
2017-10-21 13:00:45 -0400

    Gabor kernels diffrent size range bank adjusted

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-import preprocess.av.av_utils as av
+import os
+import numpy as np
+
+import path_config as pth
 import preprocess.filter.image_filters as fil
 import preprocess.image.image_utils as img
 from preprocess.common.mat_utils import Mat
-import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    file = Mat(file_name='wide_image_13.mat')
+    kernels = fil.build_filter_bank(k_size_start=2, k_size_end=6, k_step=1, gamma=0.5, sigma=0.2, lambd=0.8)
+    fil.show_kernels(kernels, save_fig=False)
+    files = os.listdir(pth.av_wide_data)
 
+    file = Mat(file_name=str('wide_image_03.mat'))
     image = file.get_image('I2')
+    f_image_arr = fil.process(255 - image[:, :, 1], kernels)
+    img.from_array(255-f_image_arr).show()
 
-    kernels = fil.build_filter_bank(k_size=24, lambd=5.01, sigma=1.79, psi=0.0, gamma=.89)
-    # image[:, :, 0] = 0
-    # image[:, :, 2] = 0
-    final_image = fil.process(image[:, :, 1], kernels)
-    plt.imshow(final_image, cmap='gray')
-    img.show_image(final_image)
-    av.show_av_graph(file, av_only=False,image_show=True)
-    plt.show()
