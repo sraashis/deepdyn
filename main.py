19a614a403b4a470ecc334dfec6d8c484a5c5d7d
Aashis <g2aashis@gmail.com>
2017-11-05 03:16:54 -0500

    New kernels.

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-import h5py as hdf
-
-import path_config as cfg
-import preprocess.av.image_filters as fil
 from commons.IMAGE import Image
+import preprocess.av.image_filters as fil
 
 if __name__ == '__main__':
-    img = Image('wide_image_03.mat')
+    img = Image('wide_image_13.mat')
     # img.show_image(image_array=img.img_array)
     img.load_kernel_bank()
     img.apply_bilateral(img.img_array[:, :, 1])
     # img.show_image(img.img_bilateral)
-    img.apply_gabor(arr=img.img_bilateral, filter_bank=img.kernel_bank)
-    img.img_gabor[img.img_gabor > 100] = 255
-    img.show_image(img.img_gabor)
-
-
+    img_temp = img.img_array[:, :, 1] - img.img_bilateral
+    img.apply_gabor(arr=img_temp, filter_bank=fil.get_chosen_gabor_bank())
+    # img.img_gabor[img.img_gabor<100] = 0
+    img.show_image(255-img.img_gabor)
+    img.show_image(img.img_array)
+    # img.show_av_graph(mat_file=img.mat, image_arr=img.img_gabor, av_only=False)
