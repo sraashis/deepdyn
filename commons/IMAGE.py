7358f6b41b441754b4b3a783d426db5bfe1ce4ec
Aashis <g2aashis@gmail.com>
2017-11-05 01:31:47 -0400

    Logger added

--- commons/IMAGE.py
+++ commons/IMAGE.py
@@ -11,16 +11,19 @@ class Image(IUtils):
     kernel_bank = None
 
     def __init__(self, av_file_name, img_key='I2'):
+        Image.log('Loading file ### ' + av_file_name)
         IUtils.__init__(self, av_file_name, img_key='I2')
         self.img_bilateral = None
         self.img_gabor = None
 
     def apply_bilateral(self, arr=None, k_size=9, sig1=75, sig2=75):
+        Image.log('Applying Bilateral filter.')
         if self.img_bilateral is not None:
             Image.warn('Bilateral filter already applied.')
         self.img_bilateral = fil.apply_bilateral(arr, k_size=k_size, sig1=sig1, sig2=sig2)
 
     def load_kernel_bank(self, kern_file_name='kernel_bank.pkl'):
+        Image.log('Loading filter kernel bank.')
         if Image.kernel_bank is not None:
             self.warn('Kernel already loaded.')
         try:
@@ -32,6 +35,7 @@ class Image(IUtils):
             jlb.dump(Image.kernel_bank, filename=kern_file_name, compress=True)
 
     def apply_gabor(self, arr=None, filter_bank=None):
+        Image.log('Applying Gabor filter.')
         self.img_gabor = fil.apply_gabor(arr, filter_bank=filter_bank)
 
     @staticmethod
