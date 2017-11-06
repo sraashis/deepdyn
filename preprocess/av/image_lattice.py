6b473ec0aa2a00625cf377236af9678e02f5621c
Aashis <g2aashis@gmail.com>
2017-11-06 17:22:06 -0500

    Cost function fixed.

--- preprocess/av/image_lattice.py
+++ preprocess/av/image_lattice.py
@@ -29,12 +29,13 @@ def assign_cost(graph=nx.Graph(), images={}, alpha=1, override=False, log=False)
         for n2 in nx.neighbors(graph, n1):
             if graph[n1][n2] == {} or override:
                 cost = 0.0
+                ix = 1
                 for weight, arr in images.items():
-                    i_diff = abs(arr[n1[0], n1[1]] - arr[n2[0], n2[1]])
-                    cost += weight * mth.pow(mth.e, (alpha * i_diff)/255)
-                    if log:
-                        print(str(i) + ':' + str(n1) + str(n2) + ' cost:' + str(cost) + ' pix-diff: ' + str(i_diff))
-                if log:
-                    print('---------------------------------------------------------------------')
+                    i_diff = abs(float(arr[n1[0], n1[1]]) - float(arr[n2[0], n2[1]]))
+                    cost += weight * mth.pow(mth.e, alpha * (i_diff/255))
+                    graph[n1][n2]['i_diff_'+str(ix)] = i_diff
+                    ix += 1
                 graph[n1][n2]['cost'] = cost
-                i += 1
+        if log:
+            print('\r' + str(i) + ': ' + str(n1), end='')
+            i += 1
