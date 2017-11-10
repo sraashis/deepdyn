b3a039d6c3998bc73eac66ddd269587eb840c12d
Aashis <g2aashis@gmail.com>
2017-11-10 16:14:34 -0500

    Test of av gabor. 4 SIZESAS

--- test/av_gabor.py
+++ test/av_gabor.py
@@ -1,22 +1,44 @@
-import networkx as nx
-import path_config as cfg
 
+# coding: utf-8
+
+# In[2]:
+
+
+import os
+import numpy as np
+
+
+# In[3]:
+
+
+os.chdir('D:\\idea projects\\pycharm projects\\ature\\')
 from commons.IMAGE import Image
-img = Image('wide_image_03.mat')
-# img.show_image(image_array=img.img_array)
-img.load_kernel_bank()
-img.apply_bilateral(img.img_array[:, :, 1])
-# img.show_image(img.img_bilateral)
-img_temp = img.img_array[:, :, 1] - img.img_bilateral
-img.apply_gabor(arr=img_temp, filter_bank=img.kernel_bank)
-img.histogram(img.img_gabor)
-# img.img_gabor[img.img_gabor<250] = 0
-# img.from_array(255-img.img_gabor)
-# img.show_av_graph(mat_file=img.mat, image_arr=img.img_gabor, av_only=False)
+import preprocess.av.image_filters as fil
+import cv2 as ocv
 
-img.create_lattice_graph(image_arr_2d=img.img_array[:,:,1])
-img.create_skeleton_by_threshold(array_2d=img.img_gabor, threshold=250)
-images = {0.5:255-img.img_skeleton, 0.25:img.img_array[:,:,1], 0.25:255-img.img_gabor}
-img.assign_cost(images=images,alpha=0.1,graph=img.lattice[0])
-nx.write_graphml(img.lattice[0], path=cfg.output_path+'\\graph\\one.graphml')
+
+# In[13]:
+
+
+img = Image('wide_image_03.mat')
+img.apply_bilateral(img.img_array[:, :, 1])
+
+
+# In[14]:
+
+
+dif_bilateral = img.get_signed_diff_int8(img.img_array[:,:,1],img.img_bilateral)
+
+
+# In[15]:
+
+
+img.show_image(255-dif_bilateral)
+
+
+# In[16]:
+
+
+img.apply_gabor(255-dif_bilateral,filter_bank=fil.get_chosen_gabor_bank())
+img.show_image(255-img.img_gabor)
 
