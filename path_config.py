988499fe2372fe1f2aeda9144ee367445e5feab5
Aashis <g2aashis@gmail.com>
2017-11-10 16:34:15 -0500

    Graph dmp path added

--- path_config.py
+++ path_config.py
@@ -11,6 +11,7 @@ path = os.path.dirname(os.path.abspath(__file__))
 input_path = join(path, 'data')
 output_path = join(path, 'out')
 kernel_dmp_path = join(output_path, 'kern')
+graph_dmp_path = join(output_path, 'graph')
 conf_path = join(path, 'conf')
 av_wide_data = join(input_path, 'av_wide_data_set')
 
