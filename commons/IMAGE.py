bd4cbf7b8df2966f08f892570d62c18a6e08199d
Aashis <g2aashis@gmail.com>
2017-11-13 18:59:26 -0500

    Logger imported from logger class.

--- commons/IMAGE.py
+++ commons/IMAGE.py
@@ -5,6 +5,7 @@ import numpy as np
 
 import path_config as cfg
 import preprocess.av.image_filters as fil
+from commons.LOGGER import Logger
 from commons.timer import check_time
 
 __all__ = [
@@ -35,9 +36,9 @@ class Image:
 
     @check_time
     def apply_bilateral(self, arr=None, k_size=41, sig1=20, sig2=20):
-        Image.log('Applying Bilateral filter.')
+        Logger.log('Applying Bilateral filter.')
         if self.img_bilateral is not None:
-            Image.warn('Bilateral filter already applied. Overriding...')
+            Logger.warn('Bilateral filter already applied. Overriding...')
         self.img_bilateral = fil.apply_bilateral(arr, k_size=k_size, sig1=sig1, sig2=sig2)
 
     @staticmethod
@@ -46,26 +47,26 @@ class Image:
 
     @check_time
     def load_kernel_bank(self, kern_file_name='kernel_bank.pkl'):
-        Image.log('Loading filter kernel bank.')
+        Logger.log('Loading filter kernel bank.')
         if Image.kernel_bank is not None:
-            self.warn('Kernel already loaded. Overriding...')
+            Logger.warn('Kernel already loaded. Overriding...')
         try:
             os.chdir(cfg.kernel_dmp_path)
             Image.kernel_bank = jlb.load(kern_file_name)
         except:
-            Image.warn('Cannot load gabor kernel from kern.pkl. Creating new and dumping.')
+            Logger.warn('Cannot load gabor kernel from kern.pkl. Creating new and dumping.')
             Image.kernel_bank = fil.get_chosen_gabor_bank()
             jlb.dump(Image.kernel_bank, filename=kern_file_name, compress=True)
 
     @check_time
     def apply_gabor(self, arr, filter_bank):
-        Image.log('Applying Gabor filter.')
+        Logger.log('Applying Gabor filter.')
         self.img_gabor = fil.apply_gabor(arr, filter_bank)
 
     def create_skeleton_by_threshold(self, array_2d=None, threshold=5):
         array_2d = 255 - array_2d
         if self.img_skeleton is not None:
-            self.warn('A skeleton already present. Overriding..')
+            Logger.warn('A skeleton already present. Overriding..')
         self.img_skeleton = np.copy(array_2d)
         self.img_skeleton[self.img_skeleton > threshold] = 255
         self.img_skeleton[self.img_skeleton <= threshold] = 0
