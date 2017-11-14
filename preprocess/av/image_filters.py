5294628be254a7cf5477750abb533ed0c8188c4d
Aashis <g2aashis@gmail.com>
2017-11-14 11:53:04 -0500

    Refactored.

--- preprocess/av/image_filters.py
+++ preprocess/av/image_filters.py
@@ -1,25 +1,5 @@
-import math as mt
-import os
-import time
-
 import cv2 as ocv
-import matplotlib.pyplot as plt
 import numpy as np
-
-import path_config as cfg
-
-
-__all__ = [
-    "build_filter_bank(k_size, sigma=2, lambd=5, gamma=0.5, psi=0,k_type=ocv.CV_32F, orientations=64)",
-    "rescale2d_unsigned(arr)",
-    "rescale3d_unsigned(arrays)",
-    "process(image, filters)",
-    "show_kernels(kernels=None, save_fig=False, file_name=str(int(time.time())))",
-    "apply_bilateral(img_arr, k_size=9, sig1=75, sig2=75)",
-    "get_signed_diff_int8(image_arr1=None, image_arr2=None)",
-    "get_chosen_gabor_bank()",
-    "apply_gabor(image_arr, filter_bank=None)"
-]
 
 
 def build_filter_bank(k_size, sigma=2, lambd=5, gamma=0.5, psi=0,
@@ -34,53 +14,8 @@ def build_filter_bank(k_size, sigma=2, lambd=5, gamma=0.5, psi=0,
     return filters
 
 
-def rescale2d_unsigned(arr):
-    m = np.max(arr)
-    n = np.min(arr)
-    return (arr - n) / (m - n)
-
-
-# Rescale 3d arrays
-def rescale3d_unsigned(arrays):
-    return list((rescale2d_unsigned(arr) for arr in arrays))
-
-
-def apply_gabor(image, filters):
-    accumulator = np.zeros_like(image)
-    for kern in filters:
-        final_image = ocv.filter2D(image, ocv.CV_8UC3, kern)
-        np.maximum(accumulator, final_image, accumulator)
-    return accumulator
-
-
-def show_kernels(kernels=None, save_fig=False, file_name=str(int(time.time()))):
-    grid_size = mt.ceil(mt.sqrt(len(kernels)))
-    for ix, kernel in enumerate(kernels):
-        plt.subplot(grid_size, grid_size, ix + 1)
-        plt.xticks([], [])
-        plt.yticks([], [])
-        plt.imshow(kernel, cmap='gray', aspect='auto')
-    if save_fig:
-        os.chdir(cfg.output_path)
-        plt.savefig(file_name + "-gabor_kernels.png")
-    else:
-        plt.show()
-
-
-def apply_bilateral(img_arr, k_size=9, sig1=75, sig2=75):
-    return ocv.bilateralFilter(img_arr, k_size, sig1, sig2)
-
-
-def get_signed_diff_int8(image_arr1=None, image_arr2=None):
-    signed_diff = np.array(image_arr1 - image_arr2, dtype=np.int8)
-    fx = np.array(signed_diff - np.min(signed_diff), np.uint8)
-    fx = rescale2d_unsigned(fx)
-    return np.array(fx * 255, np.uint8)
-
-
 def get_chosen_gabor_bank():
     kernels1 = build_filter_bank(k_size=31, gamma=0.7, lambd=5, sigma=2)
     kernels2 = build_filter_bank(k_size=31, gamma=0.7, lambd=7, sigma=3)
     kernels3 = build_filter_bank(k_size=31, gamma=0.7, lambd=10, sigma=4)
     return kernels1 + kernels2 + kernels3
-
--- preprocess/av/lattice_utils.py
+++ preprocess/av/lattice_utils.py
@@ -2,64 +2,6 @@ import math as mth
 from multiprocessing import Process
 
 import networkx as nx
-import numpy as np
-
-from commons.LOGGER import Logger
-
-__all__ = [
-    'create_lattice_graph(image_arr_2d)',
-    'assign_cost(graph=nx.Graph(), images=[()], alpha=1, override=False, log=False)',
-    'assign_node_metrics(graph=nx.Graph(), metrics=np.ndarray((0, 0)))'
-]
-
-
-def get_sub_lattice(i, j, x_block_size, y_block_size):
-    for p in range(i, i + x_block_size, 1):
-        for q in range(j, j + y_block_size, 1):
-            yield (p, q)
-
-
-def chunk_lattice(image_arr_2d, full_lattice, grid_size=(0, 0)):
-    sub_graphs_nodes = []
-    x_limit, y_limit = image_arr_2d.shape
-    x_block_size = int(x_limit / grid_size[0])
-    y_block_size = int(y_limit / grid_size[1])
-
-    remain_x = x_limit % x_block_size
-    x_end = x_limit - remain_x
-    remain_y = y_limit % y_block_size
-    y_end = y_limit - remain_y
-
-    for i in range(0, x_end, x_block_size):
-        for j in range(0, y_end, y_block_size):
-            x_size = x_block_size
-            y_size = y_block_size
-            if i + x_block_size == x_end:
-                x_size = x_block_size + remain_x
-            if j + y_block_size == y_end:
-                y_size = y_block_size + remain_y
-            Logger.log(str(i) + ',' + str(j))
-            sub_graphs_nodes.append(nx.subgraph(full_lattice, get_sub_lattice(i, j, x_size, y_size)))
-    return sub_graphs_nodes
-
-
-def generate_lattice_graph(image_arr_2d):
-    graph = nx.grid_graph([image_arr_2d.shape[0], image_arr_2d.shape[1]])
-    # for i, j in graph.nodes():
-    #     n0 = (i, j)
-    #     n1 = (i - 1, j + 1)
-    #     n2 = (i + 1, j - 1)
-    #     n3 = (i - 1, j - 1)
-    #     n4 = (i + 1, j + 1)
-    #     if n1 in graph.nodes():
-    #         graph.add_edge(n0, n1)
-    #     if n2 in graph.nodes():
-    #         graph.add_edge(n0, n2)
-    #     if n3 in graph.nodes():
-    #         graph.add_edge(n0, n3)
-    #     if n4 in graph.nodes():
-    #         graph.add_edge(n0, n4)
-    return graph
 
 
 def assign_cost(graph=nx.Graph(), images=[()], alpha=10, override=False, log=False):
@@ -88,8 +30,3 @@ def assign_cost_parallel(lattices, images=[()], alpha=10, override=False):
     for p in all_p:
         p.run()
         p.join()
-
-
-def assign_node_metrics(graph=nx.Graph(), metrics=np.ndarray((0, 0))):
-    for i, j in graph.nodes():
-        graph[(i, j)]['skeleton'] = metrics[i, j]
