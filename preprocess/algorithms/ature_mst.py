dae6dc05a40a058e8c448f5d487bcb1df1cac577
Aashis <g2aashis@gmail.com>
2017-11-18 00:23:24 -0500

    Seed number logger added.

--- preprocess/algorithms/ature_mst.py
+++ preprocess/algorithms/ature_mst.py
@@ -1,6 +1,5 @@
 from heapq import heappop, heappush
 from itertools import count
-from multiprocessing import Process
 from random import shuffle
 
 import networkx as nx
@@ -17,7 +16,7 @@ from commons.timer import check_time
 
 @check_time
 def _prim_mst(lattice=None, lattice_object=None, weight_limit_per_seed=None, weight=None, seed=None,
-              node_limit_per_seed=None):
+              node_limit_per_seed=None, number_of_seeds=50):
     if lattice.is_directed():
         raise nx.NetworkXError(
             "Minimum spanning tree not defined for directed graphs.")
@@ -26,9 +25,13 @@ def _prim_mst(lattice=None, lattice_object=None, weight_limit_per_seed=None, wei
     pop = heappop
 
     c = count()
+    seed_used = 0
     while seed:
-        print("New Seed.")
         u = seed.pop(0)
+        if seed_used >= number_of_seeds:
+            break
+        print("Seed: " + str(seed_used))
+        seed_used += 1
         node_count = 0
         seed_weight = 0.0
         frontier = []
@@ -59,7 +62,8 @@ def _prim_mst(lattice=None, lattice_object=None, weight_limit_per_seed=None, wei
                     push(frontier, (lattice[v][w].get(weight, 1), next(c), v, w))
 
 
-def run_mst(lattice_object=None, weight_limit_per_seed=20000, weight='cost', seed=None, node_limit_per_seed=10000):
+def run_mst(lattice_object=None, weight_limit_per_seed=20000, weight='cost', seed=None, node_limit_per_seed=10000,
+            number_of_seeds=50):
     shuffle(seed)
     lattice_object.accumulator = np.zeros([lattice_object.x_size, lattice_object.y_size], dtype=np.uint8)
     lattice_object.total_weight = 0.0
@@ -67,4 +71,3 @@ def run_mst(lattice_object=None, weight_limit_per_seed=20000, weight='cost', see
               weight_limit_per_seed=weight_limit_per_seed,
               weight=weight,
               seed=seed, node_limit_per_seed=node_limit_per_seed)
-
