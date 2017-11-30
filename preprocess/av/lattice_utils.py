c87af4d0c4c8096f2eb01da5f6d73473ffe58858
Aashis <g2aashis@gmail.com>
2017-11-30 18:46:39 -0500

    Removed unused node labels.

--- preprocess/av/lattice_utils.py
+++ preprocess/av/lattice_utils.py
@@ -9,12 +9,12 @@ def assign_cost(graph=nx.Graph(), images=[()], alpha=1, override=False, log=Fals
         for n2 in nx.neighbors(graph, n1):
             if graph[n1][n2] == {} or override:
                 cost = 0.0
-                ix = 1
+                # ix = 1
                 for weight, arr in images:
                     i_diff = max(arr[n1[0], n1[1]], arr[n2[0], n2[1]])
                     cost += weight * mth.pow(mth.e, alpha * (i_diff / 255))
-                    graph[n1][n2]['i_diff_' + str(ix)] = i_diff
-                    ix += 1
+                    # graph[n1][n2]['i_diff_' + str(ix)] = i_diff
+                    # ix += 1
                 graph[n1][n2]['cost'] = cost
         if log:
             print('\r' + str(i) + ': ' + str(n1), end='')
