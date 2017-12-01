966aa16fa6543b0ae94c7aa699cbae04920740cd
Aashis <g2aashis@gmail.com>
2017-11-30 22:40:14 -0500

    Algorithm for djakstra method also added for tesing.

--- preprocess/algorithms/ature_mst.py
+++ preprocess/algorithms/ature_mst.py
@@ -2,31 +2,54 @@ from heapq import heappop, heappush
 from itertools import count
 from random import shuffle
 
-import networkx as nx
 import numpy as np
 
 from commons.timer import check_time
 
-"""
-  A minimum spanning tree is a sub-graph of the graph (a tree)
-  with the minimum sum of edge weights.  A spanning forest is a
-  union of the spanning trees for each connected component of the graph.
-  """
+
+@check_time
+def _prim_mst(lattice_object=None,
+              weight=None,
+              seed=None,
+              threshold=None):
+    push = heappush
+    pop = heappop
+
+    c = count()
+    while seed:
+        u = seed.pop(0)
+        frontier = []
+        visited = [u]
+        for u, v in lattice_object.lattice.edges(u):
+            push(frontier, (lattice_object.lattice[u][v].get(weight, 1), next(c), u, v))
+
+        while frontier:
+            _, _, u, v = pop(frontier)
+
+            if v in visited:
+                continue
+
+            if lattice_object.lattice[u][v].get(weight, 1) < threshold:
+                lattice_object.accumulator[v[0], v[1]] = 255
+            else:
+                break
+
+            visited.append(v)
+
+            if v in seed:
+                seed.remove(v)
+
+            for v, w in lattice_object.lattice.edges(v):
+                if w not in visited:
+                    push(frontier, (lattice_object.lattice[v][w].get(weight, 1), next(c), v, w))
 
 
 @check_time
-def _prim_mst(lattice=None,
-              lattice_object=None,
+def _dijkstra(lattice_object=None,
               weight_limit_per_seed=None,
               weight=None,
               seed=None,
-              node_limit_per_seed=None,
               number_of_seeds=None):
-
-    if lattice.is_directed():
-        raise nx.NetworkXError(
-            "Minimum spanning tree not defined for directed graphs.")
-
     push = heappush
     pop = heappop
 
@@ -36,14 +59,12 @@ def _prim_mst(lattice=None,
         u = seed.pop(0)
         if seed_used >= number_of_seeds:
             break
-        # print("SEED: " + str(u))
         seed_used += 1
-        node_count = 0
         seed_weight = 0.0
         frontier = []
         visited = [u]
-        for u, v in lattice.edges(u):
-            push(frontier, (lattice[u][v].get(weight, 1), next(c), u, v))
+        for u, v in lattice_object.lattice.edges(u):
+            push(frontier, (lattice_object.lattice[u][v].get(weight, 1), next(c), u, v))
 
         while frontier:
             _, _, u, v = pop(frontier)
@@ -51,15 +72,12 @@ def _prim_mst(lattice=None,
             if v in visited:
                 continue
 
-            # Start with new Seed
-            if node_count > node_limit_per_seed or seed_weight > weight_limit_per_seed:
+            if seed_weight > weight_limit_per_seed:
                 break
 
-            # Keep records and control track
             lattice_object.accumulator[v[0], v[1]] = 255
             node_count += 1
-            seed_weight += float(lattice[u][v].get(weight, 1))
-            # print('\nNode Count: ' + str(node_count) + ", Total Weight: " + str(seed_weight), end='')
+            seed_weight += float(lattice_object.lattice[u][v].get(weight, 1))
 
             visited.append(v)
 
@@ -68,30 +86,32 @@ def _prim_mst(lattice=None,
                 node_count = 0
                 seed_weight = 0.0
 
-            for v, w in lattice.edges(v):
+            for v, w in lattice_object.lattice.edges(v):
                 if w not in visited:
-                    push(frontier, (lattice[v][w].get(weight, 1), next(c), v, w))
+                    push(frontier, (lattice_object.lattice[v][w].get(weight, 1), next(c), v, w))
 
 
 def run_mst(lattice_object=None,
-            weight_limit_per_seed=20000,
             weight='cost',
-            seed=None,
-            node_limit_per_seed=10000,
-            number_of_seeds=10):
-
+            seed=None, threshold=3.0):
     shuffle(seed)
     lattice_object.accumulator = np.zeros([lattice_object.x_size, lattice_object.y_size], dtype=np.uint8)
     lattice_object.total_weight = 0.0
-    _prim_mst(lattice=lattice_object.lattice,
-              lattice_object=lattice_object,
+    _prim_mst(lattice_object=lattice_object,
+              weight=weight,
+              seed=seed, threshold=threshold)
+
+
+def run_dijkstra(lattice_object=None,
+                 weight_limit_per_seed=20000,
+                 weight='cost',
+                 seed=None,
+                 number_of_seeds=10):
+    shuffle(seed)
+    lattice_object.accumulator = np.zeros([lattice_object.x_size, lattice_object.y_size], dtype=np.uint8)
+    lattice_object.total_weight = 0.0
+    _dijkstra(lattice_object=lattice_object,
               weight_limit_per_seed=weight_limit_per_seed,
               weight=weight,
               seed=seed,
-              node_limit_per_seed=node_limit_per_seed,
               number_of_seeds=number_of_seeds)
-
-# Start with one seed.
-# Weight limit per seed.
-# weight limit for whole run.
-#
