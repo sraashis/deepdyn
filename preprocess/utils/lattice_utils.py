d10fcb523756eac25aa5fa72e57788ec405f4933
Aashis <g2aashis@gmail.com>
2017-12-03 22:10:26 -0500

    Refactored.

--- preprocess/utils/lattice_utils.py
+++ preprocess/utils/lattice_utils.py
@@ -3,22 +3,15 @@ import math as mth
 import networkx as nx
 
 
-def assign_cost(graph=nx.Graph(), images=[()], alpha=1, override=False, log=False):
-    i = 0
+def assign_cost(graph=nx.Graph(), images=[()], alpha=1, override=False):
     for n1 in graph.nodes():
         for n2 in nx.neighbors(graph, n1):
             if graph[n1][n2] == {} or override:
                 cost = 0.0
-                # ix = 1
                 for weight, arr in images:
-                    i_diff = max(arr[n1[0], n1[1]], arr[n2[0], n2[1]])
-                    cost += weight * mth.pow(mth.e, alpha * (i_diff / 255))
-                    # graph[n1][n2]['i_diff_' + str(ix)] = i_diff
-                    # ix += 1
+                    m = max(arr[n1[0], n1[1]], arr[n2[0], n2[1]])
+                    cost += weight * mth.pow(mth.e, alpha * (m / 255))
                 graph[n1][n2]['cost'] = cost
-        if log:
-            print('\r' + str(i) + ': ' + str(n1), end='')
-            i += 1
 
 
 def get_seed_node_list(image_array_2d=None):
