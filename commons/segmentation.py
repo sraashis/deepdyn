ef697802774d05c51b5c46fa7cf42ec02898efda
Aashis <g2aashis@gmail.com>
2018-02-21 01:22:07 -0500

    DivideByZero ex fixed in scores calculation.

--- commons/segmentation.py
+++ commons/segmentation.py
@@ -128,7 +128,7 @@ class AtureTest:
                     fn += 1
                 if arr_2d[i, j] == 0 and truth[i, j] == 0:
                     tn += 1
-        p, r, a = 0, 0, 0
+        p, r, a, f1 = 0, 0, 0, 0
         try:
             p = tp / (tp + fp)
         except ZeroDivisionError:
@@ -144,11 +144,16 @@ class AtureTest:
         except ZeroDivisionError:
             a = 0
 
+        try:
+            f1 = 2 * p * r / (p + r)
+        except ZeroDivisionError:
+            f1 = 0
+
         return {
             'Precision': p,
             'Recall': r,
             'Accuracy': a,
-            'F1': 2 * p * r / (p + r)
+            'F1': f1
         }
 
     def _run(self, accumulator=None, params={},
@@ -239,7 +244,6 @@ class AtureTest:
         params['alpha'] -= alpha_decay
         params['sk_threshold'] = 100
 
-    @checktime
     def _save(self, accumulator=None, params=None, epoch=None, save_images=False):
         i = next(self.c)
         base = 'scores' + str(epoch)
