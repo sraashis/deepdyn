f420a70777f06453711d3da2b2a3c35f57d5de56
Aashis <g2aashis@gmail.com>
2018-02-22 19:59:43 -0500

    Epoch tested.

--- commons/timer.py
+++ commons/timer.py
@@ -4,7 +4,7 @@ import time
 def checktime(func):
     def inner(*args, **kwargs):
         start = time.time()
-        print(func.__name__ + '(__)...', end='')
+        print('"' + func.__name__ + '(__)" ...', end='')
         result = func(*args, **kwargs)
         print(' took ' + str(
             round(time.time() - start, 3)) + " seconds")
