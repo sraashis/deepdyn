13ffe8209bbe54aaf279be44e5e97e5d28c302ee
Aashis <g2aashis@gmail.com>
2018-03-10 04:12:25 -0500

    Accumulator to image.

--- preprocess/algorithms/fast_mst.py
+++ preprocess/algorithms/fast_mst.py
@@ -7,17 +7,14 @@ from commons.timer import checktime
 
 
 @checktime
-def run_segmentation(accumulator_2d=None, image_obj=None,
+def run_segmentation(accumulator_2d=None, images_used=None,
                      seed_list=None,
-                     params=None):
-    graph = image_obj.graph.copy()
-    img_used = [(params['gabor_contrib'], image_obj.img_gabor),
-                (1 - params['gabor_contrib'], image_obj.working_arr)]
+                     params=None, graph=None):
 
     edges_to_delete = []
     for e in graph.edges():
         cost = 0.0
-        for weight, arr in img_used:
+        for weight, arr in images_used:
             i_diff = max(arr[e[0]], arr[e[1]])
             cost += weight * mth.pow(mth.e, params['alpha'] * (i_diff / 255))
             graph[e[0]][e[1]][const.GRAPH_WEIGHT_METRICS] = cost
@@ -30,5 +27,3 @@ def run_segmentation(accumulator_2d=None, image_obj=None,
         if component.isdisjoint(seed_list) is False:
             for node in component:
                 accumulator_2d[node] = 255
-
-    return graph
