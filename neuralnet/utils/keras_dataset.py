db2fbb80e178ec76c4dc0187a8434bccc3e4b02f
Aashis <g2aashis@gmail.com>
2018-04-13 19:40:30 -0400

    Removed 2 way classification codes.

--- neuralnet/utils/keras_dataset.py
+++ neuralnet/utils/keras_dataset.py
@@ -7,14 +7,14 @@ import keras
 import numpy as np
 from keras.preprocessing.image import ImageDataGenerator
 
-import neuralnet.utils.data_utils as datautils
 import utils.img_utils as imgutil
 from commons.IMAGE import Image
 
 
 class KerasPatchesGenerator(keras.utils.Sequence):
     def __init__(self, Dirs=None, batch_size=None, patch_size=None, num_classes=None,
-                 fget_mask=None, fget_truth=None, fget_segmented=None, img_generator=ImageDataGenerator(), shuffle= False):
+                 fget_mask=None, fget_truth=None, img_generator=ImageDataGenerator(),
+                 shuffle=False):
 
         """
             :param Dirs: Should contain images, mask, truth, and seegmented path.(segmented only for 4 way classification)
@@ -23,7 +23,6 @@ class KerasPatchesGenerator(keras.utils.Sequence):
             :param transform:
             :param fget_mask: mask file getter
             :param fget_truth: ground truth file getter
-            :param fget_segmented: segmented file getter
         """
 
         self.patch_size = patch_size
@@ -33,7 +32,7 @@ class KerasPatchesGenerator(keras.utils.Sequence):
         self.file_names = os.listdir(Dirs['images'])
         self.images = {}
         self.img_generator = img_generator
-        self.shuffle = False
+        self.shuffle = shuffle
         for ID, img_file in enumerate(self.file_names):
 
             img_obj = Image()
@@ -44,19 +43,10 @@ class KerasPatchesGenerator(keras.utils.Sequence):
             img_obj.load_mask(mask_dir=Dirs['mask'], fget_mask=fget_mask, erode=True)
             img_obj.load_ground_truth(gt_dir=Dirs['truth'], fget_ground_truth=fget_truth)
 
-            if self.num_classes == 4:
-                segmented_file = os.path.join(Dirs['segmented'], fget_segmented(img_file))
-                img_obj.res['segmented'] = imgutil.get_image_as_array(segmented_file, channels=1)
-
             for i, j in itertools.product(np.arange(img_obj.working_arr.shape[0]),
                                           np.arange(img_obj.working_arr.shape[1])):
                 if img_obj.mask[i, j] == 255:
-                    y = None
-                    if self.num_classes == 2:
-                        y = 1 if img_obj.ground_truth[i, j] == 255 else 0
-                    elif self.num_classes == 4:
-                        y = datautils.get_lable(i, j, img_obj.res['segmented'], img_obj.ground_truth)
-                    self.IDs.append([ID, i, j, y])
+                    self.IDs.append([ID, i, j, 1 if img_obj.ground_truth[i, j] == 255 else 0])
 
             self.images[ID] = img_obj
         print('### ' + str(self.__len__()) + ' batches found.')
@@ -84,7 +74,7 @@ class KerasPatchesGenerator(keras.utils.Sequence):
                         1] > patch_j >= 0:
                         patch[k_half + k, k_half + l] = self.images[ID].working_arr[patch_i, patch_j]
 
-            batch[ix, ] = patch[..., None]
+            batch[ix,] = patch[..., None]
             batch_y[ix] = y
 
         self.img_generator.fit(batch)
