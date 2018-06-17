9d8c986c4c8c7173c5c2eff014cd0423c75ae9bf
Aashis Khanal <aturegsu@gmail.com>
2018-06-17 09:44:11 -0400

    Loggin enabled

--- testing/unet_entry.py
+++ testing/unet_entry.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-from .unet_runner import train, run_tests
+from .unet_runner import UnetRunner
 
 sys.path.append('/home/akhanal1/ature')
 os.chdir('/home/akhanal1/ature')
@@ -11,117 +11,121 @@ os.chdir('/home/akhanal1/ature')
 import torchvision.transforms as transforms
 from time import time
 
-sep = os.sep
-Params = {}
-Params['num_channels'] = 1
-Params['classes'] = {'background': 0, 'vessel': 1, }
-Params['batch_size'] = 4
-Params['num_classes'] = len(Params['classes'])
-Params['epochs'] = 1000
-Params['patch_size'] = (388, 388)  # rows X cols
-Params['use_gpu'] = True
-Params['learning_rate'] = 0.0005
-Params['checkpoint_dir'] = 'assests' + sep + 'nnet_models'
+if __name__ == "__main__":
+    sep = os.sep
+    Params = {}
+    Params['num_channels'] = 1
+    Params['classes'] = {'background': 0, 'vessel': 1, }
+    Params['batch_size'] = 4
+    Params['num_classes'] = len(Params['classes'])
+    Params['epochs'] = 1000
+    Params['patch_size'] = (388, 388)  # rows X cols
+    Params['use_gpu'] = True
+    Params['learning_rate'] = 0.0005
+    Params['checkpoint_dir'] = 'assests' + sep + 'nnet_models'
 
-transform = transforms.Compose([
-    transforms.ToPILImage(),
-    transforms.ToTensor()
-])
+    transform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.ToTensor()
+    ])
 
-##################### DRIVE DATASET ########################
-Dirs = {}
-Dirs['data'] = 'data' + sep + 'DRIVE' + sep + 'training'
-Dirs['images'] = Dirs['data'] + sep + 'images'
-Dirs['mask'] = Dirs['data'] + sep + 'mask'
-Dirs['truth'] = Dirs['data'] + sep + '1st_manual'
+    ##################### DRIVE DATASET ########################
+    Dirs = {}
+    Dirs['data'] = 'data' + sep + 'DRIVE' + sep + 'training'
+    Dirs['images'] = Dirs['data'] + sep + 'images'
+    Dirs['mask'] = Dirs['data'] + sep + 'mask'
+    Dirs['truth'] = Dirs['data'] + sep + '1st_manual'
 
-TestDirs = {}
-TestDirs['data'] = 'data' + sep + 'DRIVE' + sep + 'testing'
-TestDirs['images'] = TestDirs['data'] + sep + 'images'
-TestDirs['mask'] = TestDirs['data'] + sep + 'mask'
-TestDirs['truth'] = TestDirs['data'] + sep + '1st_manual'
-TestDirs['segmented'] = TestDirs['data'] + sep + 'segmented'
+    TestDirs = {}
+    TestDirs['data'] = 'data' + sep + 'DRIVE' + sep + 'testing'
+    TestDirs['images'] = TestDirs['data'] + sep + 'images'
+    TestDirs['mask'] = TestDirs['data'] + sep + 'mask'
+    TestDirs['truth'] = TestDirs['data'] + sep + '1st_manual'
+    TestDirs['segmented'] = TestDirs['data'] + sep + 'segmented'
 
-ValidationDirs = {}
-ValidationDirs['data'] = 'data' + sep + 'DRIVE' + sep + 'testing'
-ValidationDirs['images'] = ValidationDirs['data'] + sep + 'validation_images'
-ValidationDirs['mask'] = ValidationDirs['data'] + sep + 'mask'
-ValidationDirs['truth'] = ValidationDirs['data'] + sep + '1st_manual'
+    ValidationDirs = {}
+    ValidationDirs['data'] = 'data' + sep + 'DRIVE' + sep + 'testing'
+    ValidationDirs['images'] = ValidationDirs['data'] + sep + 'validation_images'
+    ValidationDirs['mask'] = ValidationDirs['data'] + sep + 'mask'
+    ValidationDirs['truth'] = ValidationDirs['data'] + sep + '1st_manual'
 
-for k, folder in Dirs.items():
-    os.makedirs(folder, exist_ok=True)
-for k, folder in TestDirs.items():
-    os.makedirs(folder, exist_ok=True)
-for k, folder in ValidationDirs.items():
-    os.makedirs(folder, exist_ok=True)
-
-
-def get_mask_file(file_name):
-    return file_name.split('_')[0] + '_training_mask.gif'
+    for k, folder in Dirs.items():
+        os.makedirs(folder, exist_ok=True)
+    for k, folder in TestDirs.items():
+        os.makedirs(folder, exist_ok=True)
+    for k, folder in ValidationDirs.items():
+        os.makedirs(folder, exist_ok=True)
 
 
-def get_ground_truth_file(file_name):
-    return file_name.split('_')[0] + '_manual1.gif'
+    def get_mask_file(file_name):
+        return file_name.split('_')[0] + '_training_mask.gif'
 
 
-def get_mask_file_test(file_name):
-    return file_name.split('_')[0] + '_test_mask.gif'
+    def get_ground_truth_file(file_name):
+        return file_name.split('_')[0] + '_manual1.gif'
 
 
-check_point_file = "{}-".format(time()) + 'chkDRIVEunet.tar'
-train(Params=Params, Dirs=Dirs, ValidationDirs=ValidationDirs, transform=transforms,
-      train_mask_getter=get_mask_file, train_groundtruth_getter=get_ground_truth_file,
-      val_mask_getter=get_mask_file_test, val_groundtruth_getter=get_ground_truth_file,
-      check_point_file=check_point_file)
-
-run_tests(TestDirs=TestDirs, Params=Params, transform=transform, test_mask_getter=get_mask_file_test,
-          test_groundtruth_file_getter=get_ground_truth_file, checkpoint_file=check_point_file)
-#################################################################################
+    def get_mask_file_test(file_name):
+        return file_name.split('_')[0] + '_test_mask.gif'
 
 
-############## AV-WIDE Dataset ##################################################
-Dirs = {}
-Dirs['checkpoint'] = 'assests' + sep + 'nnet_models'
-Dirs['data'] = 'data' + sep + 'AV-WIDE' + sep + 'training'
-Dirs['images'] = Dirs['data'] + sep + 'images'
-Dirs['mask'] = Dirs['data'] + sep + 'mask'
-Dirs['truth'] = Dirs['data'] + sep + '1st_manual'
+    runner = UnetRunner(Params=Params,
+                        transform=transform,
+                        checkpoint_file="{}-".format(time()) + 'chkDRIVEunet.tar')
 
-TestDirs = {}
-TestDirs['data'] = 'data' + sep + 'AV-WIDE' + sep + 'testing'
-TestDirs['images'] = TestDirs['data'] + sep + 'images'
-TestDirs['mask'] = TestDirs['data'] + sep + 'mask'
-TestDirs['truth'] = TestDirs['data'] + sep + '1st_manual'
-TestDirs['segmented'] = TestDirs['data'] + sep + 'segmented'
+    runner.train(Dirs=Dirs, ValidationDirs=ValidationDirs, transform=transforms,
+                 train_mask_getter=get_mask_file, train_groundtruth_getter=get_ground_truth_file,
+                 val_mask_getter=get_mask_file_test, val_groundtruth_getter=get_ground_truth_file)
 
-ValidationDirs = {}
-ValidationDirs['data'] = 'data' + sep + 'AV-WIDE' + sep + 'testing'
-ValidationDirs['images'] = ValidationDirs['data'] + sep + 'validation_images'
-ValidationDirs['mask'] = ValidationDirs['data'] + sep + 'mask'
-ValidationDirs['truth'] = ValidationDirs['data'] + sep + '1st_manual'
+    runner.run_tests(TestDirs=TestDirs, transform=transform, test_mask_getter=get_mask_file_test,
+                     test_groundtruth_file_getter=get_ground_truth_file)
+    #################################################################################
 
-for k, folder in Dirs.items():
-    os.makedirs(folder, exist_ok=True)
-for k, folder in TestDirs.items():
-    os.makedirs(folder, exist_ok=True)
-for k, folder in ValidationDirs.items():
-    os.makedirs(folder, exist_ok=True)
+    ############## AV-WIDE Dataset ##################################################
+    Dirs = {}
+    Dirs['checkpoint'] = 'assests' + sep + 'nnet_models'
+    Dirs['data'] = 'data' + sep + 'AV-WIDE' + sep + 'training'
+    Dirs['images'] = Dirs['data'] + sep + 'images'
+    Dirs['mask'] = Dirs['data'] + sep + 'mask'
+    Dirs['truth'] = Dirs['data'] + sep + '1st_manual'
+
+    TestDirs = {}
+    TestDirs['data'] = 'data' + sep + 'AV-WIDE' + sep + 'testing'
+    TestDirs['images'] = TestDirs['data'] + sep + 'images'
+    TestDirs['mask'] = TestDirs['data'] + sep + 'mask'
+    TestDirs['truth'] = TestDirs['data'] + sep + '1st_manual'
+    TestDirs['segmented'] = TestDirs['data'] + sep + 'segmented'
+
+    ValidationDirs = {}
+    ValidationDirs['data'] = 'data' + sep + 'AV-WIDE' + sep + 'testing'
+    ValidationDirs['images'] = ValidationDirs['data'] + sep + 'validation_images'
+    ValidationDirs['mask'] = ValidationDirs['data'] + sep + 'mask'
+    ValidationDirs['truth'] = ValidationDirs['data'] + sep + '1st_manual'
+
+    for k, folder in Dirs.items():
+        os.makedirs(folder, exist_ok=True)
+    for k, folder in TestDirs.items():
+        os.makedirs(folder, exist_ok=True)
+    for k, folder in ValidationDirs.items():
+        os.makedirs(folder, exist_ok=True)
 
 
-def get_mask_file(file_name):
-    return file_name.split('_')[0] + '_training_mask.gif'
+    def get_mask_file(file_name):
+        return file_name.split('_')[0] + '_training_mask.gif'
 
 
-def get_ground_truth_file(file_name):
-    return file_name.split('.')[0] + '_vessels.png'
+    def get_ground_truth_file(file_name):
+        return file_name.split('.')[0] + '_vessels.png'
 
 
-check_point_file = "{}-".format(time()) + 'chkWIDEunet.tar'
-train(Params=Params, Dirs=Dirs, ValidationDirs=ValidationDirs, transform=transforms,
-      train_mask_getter=get_mask_file, train_groundtruth_getter=get_ground_truth_file,
-      val_mask_getter=get_mask_file_test, val_groundtruth_getter=get_ground_truth_file,
-      check_point_file=check_point_file)
+    runner = UnetRunner(Params=Params,
+                        transform=transform,
+                        checkpoint_file="{}-".format(time()) + 'chkWIDEunet.tar')
 
-run_tests(TestDirs=TestDirs, Params=Params, transform=transform, test_mask_getter=get_mask_file_test,
-          test_groundtruth_file_getter=get_ground_truth_file, checkpoint_file=check_point_file)
-######################################################################################
+    runner.train(Dirs=Dirs, ValidationDirs=ValidationDirs, transform=transforms,
+                 train_mask_getter=get_mask_file, train_groundtruth_getter=get_ground_truth_file,
+                 val_mask_getter=get_mask_file_test, val_groundtruth_getter=get_ground_truth_file)
+
+    runner.run_tests(TestDirs=TestDirs, transform=transform, test_mask_getter=get_mask_file_test,
+                     test_groundtruth_file_getter=get_ground_truth_file)
+    ######################################################################################
--- testing/unet_runner.py
+++ testing/unet_runner.py
@@ -14,78 +14,82 @@ from neuralnet.unet.unet_trainer import UNetNNTrainer
 sep = os.sep
 
 
-def train(Params, Dirs, ValidationDirs, transform, train_mask_getter, train_groundtruth_getter,
-          val_mask_getter,
-          val_groundtruth_getter, check_point_file):
-    try:
-        torch.cuda.empty_cache()
-    except Exception:
-        pass
+class UnetRunner():
+    def __init__(self, Params, transform, checkpoint_file):
+        self.Params = Params
+        self.transform = transform
+        self.checkpoint_file = checkpoint_file
+        self.model = UNet(self.Params['num_channels'], self.Params['num_classes'])
 
-    # """
-    # ### Load train data
-    trainset = PatchesGenerator(Dirs=Dirs, train_image_size=Params['patch_size'],
-                                transform=transform,
-                                fget_mask=train_mask_getter,
-                                fget_truth=train_groundtruth_getter)
+    def train(self, Dirs, ValidationDirs, transform, train_mask_getter, train_groundtruth_getter,
+              val_mask_getter, val_groundtruth_getter):
+        try:
+            torch.cuda.empty_cache()
+        except Exception:
+            pass
 
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=Params['batch_size'], shuffle=False, num_workers=3)
+        # ### Load train data
+        trainset = PatchesGenerator(Dirs=Dirs, train_image_size=self.Params['patch_size'],
+                                    transform=transform,
+                                    fget_mask=train_mask_getter,
+                                    fget_truth=train_groundtruth_getter)
 
-    # ### Load Validation Data
-    validation_set = PatchesGenerator(Dirs=ValidationDirs, train_image_size=Params['patch_size'],
-                                      transform=transform,
-                                      fget_mask=val_mask_getter,
-                                      fget_truth=val_groundtruth_getter)
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=self.Params['batch_size'], shuffle=False,
+                                                  num_workers=3)
 
-    validationloader = torch.utils.data.DataLoader(validation_set, batch_size=Params['batch_size'], shuffle=False,
-                                                   num_workers=3)
+        # ### Load Validation Data
+        validation_set = PatchesGenerator(Dirs=ValidationDirs, train_image_size=self.Params['patch_size'],
+                                          transform=transform,
+                                          fget_mask=val_mask_getter,
+                                          fget_truth=val_groundtruth_getter)
 
-    # """
-    # ### Define the network
-    net = UNet(Params['num_channels'], Params['num_classes'])
-    optimizer = optim.Adam(net.parameters(), lr=Params['learning_rate'])
+        validationloader = torch.utils.data.DataLoader(validation_set, batch_size=self.Params['batch_size'],
+                                                       shuffle=False,
+                                                       num_workers=3)
 
-    # ### Train and evaluate network
-    trainer = UNetNNTrainer(model=net, checkpoint_dir=Params['checkpoint_dir'],
-                            checkpoint_file='TRAIN-' + check_point_file,
-                            log_to_file=True,
-                            use_gpu=Params['use_gpu'])
-    trainer.train(optimizer=optimizer, dataloader=trainloader, epochs=Params['epochs'],
-                  validationloader=validationloader, force_checkpoint=False, log_frequency=20)
+        # ### Define the network
+        optimizer = optim.Adam(self.model.parameters(), lr=self.Params['learning_rate'])
 
-    with open(os.path.join(Params['checkpoint_dir']) + sep + 'PARAMS-' + check_point_file + '.txt') as pfile:
-        pfile.write(Params)
+        # ### Train and evaluate network
+        trainer = UNetNNTrainer(model=self.model, checkpoint_dir=self.Params['checkpoint_dir'],
+                                checkpoint_file='TRAIN-' + self.checkpoint_file,
+                                log_to_file=True,
+                                use_gpu=self.Params['use_gpu'])
+        trainer.train(optimizer=optimizer, dataloader=trainloader, epochs=self.Params['epochs'],
+                      validationloader=validationloader, force_checkpoint=False, log_frequency=20)
 
+        with open(
+                os.path.join(self.Params['checkpoint_dir']) + sep + 'PARAMS-' + self.checkpoint_file + '.txt', 'w') as pfile:
+            pfile.write(str(self.Params))
 
-def run_tests(TestDirs, Params, transform, test_mask_getter, test_groundtruth_file_getter, checkpoint_file):
-    # ### Define the network
-    net = UNet(Params['num_channels'], Params['num_classes'])
-    trainer = UNetNNTrainer(model=net, checkpoint_dir=Params['checkpoint_dir'],
-                            checkpoint_file='TEST-' + checkpoint_file,
-                            log_to_file=True,
-                            use_gpu=Params['use_gpu'])
-    trainer.resume_from_checkpoint()
-    for filename in os.listdir(TestDirs['images']):
-        img_obj = SegmentedImage()
-        img_obj.load_file(data_dir=TestDirs['images'], file_name=filename)
-        img_obj.load_mask(mask_dir=TestDirs['mask'], fget_mask=test_mask_getter, erode=True)
-        img_obj.load_ground_truth(gt_dir=TestDirs['truth'], fget_ground_truth=test_groundtruth_file_getter)
+    def run_tests(self, TestDirs, transform, test_mask_getter, test_groundtruth_file_getter):
+        # ### Define the network
+        trainer = UNetNNTrainer(model=self.model, checkpoint_dir=self.Params['checkpoint_dir'],
+                                checkpoint_file='TEST-' + self.checkpoint_file,
+                                log_to_file=True,
+                                use_gpu=self.Params['use_gpu'])
+        trainer.resume_from_checkpoint()
+        for filename in os.listdir(TestDirs['images']):
+            img_obj = SegmentedImage()
+            img_obj.load_file(data_dir=TestDirs['images'], file_name=filename)
+            img_obj.load_mask(mask_dir=TestDirs['mask'], fget_mask=test_mask_getter, erode=True)
+            img_obj.load_ground_truth(gt_dir=TestDirs['truth'], fget_ground_truth=test_groundtruth_file_getter)
 
-        testset = PatchesGeneratorPerImgObj(img_obj=img_obj, train_image_size=Params['patch_size'],
-                                            transform=transform)
+            testset = PatchesGeneratorPerImgObj(img_obj=img_obj, train_image_size=self.Params['patch_size'],
+                                                transform=transform)
 
-        testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                                 shuffle=False, num_workers=0, sampler=None)
-        scores, y_pred, y_true = trainer.evaluate(dataloader=testloader, force_checkpoint=False)
-        ppp = ut.merge_patches(scores, img_obj.working_arr.shape, Params['patch_size'])
-        IMG.fromarray(ppp).save(TestDirs['segmented'] + sep + filename + '.png')
+            testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                                     shuffle=False, num_workers=0, sampler=None)
+            scores, y_pred, y_true = trainer.evaluate(dataloader=testloader, force_checkpoint=False)
+            ppp = ut.merge_patches(scores, img_obj.working_arr.shape, self.Params['patch_size'])
+            IMG.fromarray(ppp).save(TestDirs['segmented'] + sep + filename + '.png')
 
-# ### FAST MST algorithm
-# params = {'sk_threshold': 150,
-#           'alpha': 7.0,
-#           'orig_contrib': 0.3,
-#           'seg_threshold': 24}
-#
-# img_obj.working_arr = None  # todo
-# img_obj.generate_skeleton(threshold=params['sk_threshold'])
-# img_obj.generate_lattice_graph()
+    # ### FAST MST algorithm
+    # params = {'sk_threshold': 150,
+    #           'alpha': 7.0,
+    #           'orig_contrib': 0.3,
+    #           'seg_threshold': 24}
+    #
+    # img_obj.working_arr = None  # todo
+    # img_obj.generate_skeleton(threshold=params['sk_threshold'])
+    # img_obj.generate_lattice_graph()
