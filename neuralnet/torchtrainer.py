549f128a97eb8df8d017d68cba4cfe92342f2675
Aashis Khanal <aturegsu@gmail.com>
2018-07-10 23:40:02 -0400

    Added DRIVE and WIDE dataset.

--- neuralnet/torchtrainer.py
+++ neuralnet/torchtrainer.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from time import time
 
 import torch
@@ -126,8 +127,11 @@ class NNTrainer:
 
     @staticmethod
     def get_logger(log_file):
-        assert (os.path.isfile(
-            os.path.join('net_logs', log_file)) is False), log_file + " already present. RENAME OR DELETE "
+
+        if os.path.isfile(os.path.join('net_logs', log_file)):
+            print('### CRITICAL!!! "net_logs/' + log_file + '" already exists. Rename or delete to proceed.')
+            sys.exit(1)
+
         file = open(os.path.join('net_logs', log_file), 'w')
         NNTrainer.flush(file, 'ID,TYPE,EPOCH,BATCH,PRECISION,RECALL,F1,ACCURACY,LOSS')
         return file
