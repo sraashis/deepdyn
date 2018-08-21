42b52f36fc0ea2e284da017cc8e582b2d7251ae6
Aashis Khanal <aturegsu@gmail.com>
2018-08-21 09:38:56 -0400

    Thnet trainining with flipped data.

--- neuralnet/thrnet/thrnet.py
+++ neuralnet/thrnet/thrnet.py
@@ -35,7 +35,7 @@ class ThrNet(nn.Module):
         self.mxp_kern_size = 2
         self.mxp_stride = 2
         self.pool3 = nn.MaxPool2d(kernel_size=self.mxp_kern_size, stride=self.mxp_stride)
-        self.conv3 = nn.Conv2d(128, 256, self.kern_size,
+        self.conv3 = nn.Conv2d(128, 512, self.kern_size,
                                stride=self.kern_stride, padding=self.kern_padding)
         self._update_output_size()
 
@@ -45,7 +45,7 @@ class ThrNet(nn.Module):
         self.mxp_kern_size = 1
         self.mxp_stride = 1
         self.pool4 = nn.MaxPool2d(kernel_size=self.mxp_kern_size, stride=self.mxp_stride)
-        self.conv4 = nn.Conv2d(256, 128, self.kern_size,
+        self.conv4 = nn.Conv2d(512, 1024, self.kern_size,
                                stride=self.kern_stride, padding=self.kern_padding)
         self._update_output_size()
 
@@ -55,13 +55,15 @@ class ThrNet(nn.Module):
         self.mxp_kern_size = 2
         self.mxp_stride = 2
         self.pool5 = nn.MaxPool2d(kernel_size=self.mxp_kern_size, stride=self.mxp_stride)
-        self.conv5 = nn.Conv2d(128, 64, self.kern_size,
+        self.conv5 = nn.Conv2d(1024, 128, self.kern_size,
                                stride=self.kern_stride, padding=self.kern_padding)
         self._update_output_size()
 
-        self.linearWidth = 64 * int(self.width) * int(self.width)
-        self.fc1 = nn.Linear(self.linearWidth, 16)
-        self.fc2 = nn.Linear(16, 1)
+        self.linearWidth = 128 * int(self.width) * int(self.width)
+        self.fc1 = nn.Linear(self.linearWidth, 512)
+        self.fc2 = nn.Linear(512, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, 1)
 
     def forward(self, x):
         x = self.pool1(F.relu(self.conv1(x)))
@@ -72,10 +74,11 @@ class ThrNet(nn.Module):
         x = F.dropout2d(x, p=0.3)
         x = self.pool5(F.relu(self.conv5(x)))
         x = x.view(-1, self.linearWidth)
-        x = F.dropout2d(x, p=0.4)
         x = F.relu(self.fc1(x))
-        x = F.dropout2d(x, p=0.4)
-        x = self.fc2(x)
+        x = F.dropout2d(x, p=0.3)
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
         return x
 
     def _update_output_size(self):
@@ -84,3 +87,6 @@ class ThrNet(nn.Module):
         temp1 = self.width
         self.width = ((self.width - self.mxp_kern_size) / self.mxp_stride) + 1
         print('Output width[ ' + str(temp) + ' -conv-> ' + str(temp1) + ' -maxpool-> ' + str(self.width) + ' ]')
+
+
+f = ThrNet(31, 1)
