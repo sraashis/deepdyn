0060ea12a0507a7431472191c1ef7c5dc6217e30
Aashis Khanal <aturegsu@gmail.com>
2018-09-17 16:12:05 -0400

    NEW Arch

--- neuralnet/thrnet/inception.py
+++ neuralnet/thrnet/inception.py
@@ -66,10 +66,10 @@ class InceptionThrNet(nn.Module):
 
         # We will crop and concat from inception1 to this layer
         self.inception2 = Inception(width=width, in_ch=32, out_ch=64)
-        self.inception3 = Inception(width=width, in_ch=64, out_ch=64)
+        self.inception3 = Inception(width=width, in_ch=64, out_ch=32)
         self.inception3_mxp = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
 
-        self.inception4 = Inception(width=width, in_ch=64, out_ch=32)
+        self.inception4 = Inception(width=width, in_ch=32, out_ch=32)
         self.inception4_mxp = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
 
         self.inception5 = Inception(width=width, in_ch=32, out_ch=16)
