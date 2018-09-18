90358eef280fa57b4a3d7e0ff011bd7b1a73211f
Aashis Khanal <aturegsu@gmail.com>
2018-09-18 18:57:31 -0400

    ESTIMATE->REMOVE SMALL CONNECTED COMPONENTS->GET EACH 10 patch and use it to train, validate and test.

--- neuralnet/thrnet/inception.py
+++ neuralnet/thrnet/inception.py
@@ -61,20 +61,17 @@ class InceptionThrNet(nn.Module):
     def __init__(self, width, input_ch, num_class):
         super(InceptionThrNet, self).__init__()
 
-        self.inception1 = Inception(width=width, in_ch=input_ch, out_ch=16)
+        self.inception1 = Inception(width=width, in_ch=input_ch, out_ch=32)
         self.inception1_mxp = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
 
         # We will crop and concat from inception1 to this layer
-        self.inception2 = Inception(width=width, in_ch=32, out_ch=32)
+        self.inception2 = Inception(width=width, in_ch=64, out_ch=32)
         self.inception2_mxp = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
 
-        self.inception3 = Inception(width=width, in_ch=32, out_ch=32)
-        self.inception4 = Inception(width=width, in_ch=32, out_ch=32)
-        self.inception4_mxp = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.inception3 = Inception(width=width, in_ch=32, out_ch=64)
+        self.inception4 = Inception(width=width, in_ch=64, out_ch=32)
 
-        self.inception5 = Inception(width=width, in_ch=32, out_ch=32)
-
-        self.linearWidth = 32 * 4 * 4
+        self.linearWidth = 32 * 8 * 8
         self.fc1_out = nn.Linear(self.linearWidth, 512)
         self.fc2_out = nn.Linear(512, 64)
         self.fc3_out = nn.Linear(64, num_class)
@@ -89,11 +86,9 @@ class InceptionThrNet(nn.Module):
 
         i3_out = self.inception3(i2_dwn_out)
         i4_out = self.inception4(i3_out)
-        i4_dwn_out = self.inception4_mxp(i4_out)
 
-        i5_out = self.inception5(i4_dwn_out)
-
-        flattened = i5_out.view(-1, self.linearWidth)
+        flattened = i4_out.view(-1, self.linearWidth)
+        # flattened = F.dropout2d(flattened, p=0.4)
         fc1_out = F.relu(self.fc1_out(flattened))
         fc2_out = F.relu(self.fc2_out(fc1_out))
 
