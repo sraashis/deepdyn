e261c8a2430e7997a02045ecdb11c958ba7e431c
Aashis Khanal <aturegsu@gmail.com>
2018-09-18 19:57:37 -0400

    <>RUNNING<>ESTIMATE->REMOVE SMALL CONNECTED COMPONENTS->GET EACH 10 patch and use it to train, validate and test.

--- neuralnet/thrnet/inception.py
+++ neuralnet/thrnet/inception.py
@@ -61,17 +61,21 @@ class InceptionThrNet(nn.Module):
     def __init__(self, width, input_ch, num_class):
         super(InceptionThrNet, self).__init__()
 
-        self.inception1 = Inception(width=width, in_ch=input_ch, out_ch=32)
+        self.inception1 = Inception(width=width, in_ch=input_ch, out_ch=64)
         self.inception1_mxp = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
 
         # We will crop and concat from inception1 to this layer
-        self.inception2 = Inception(width=width, in_ch=64, out_ch=32)
-        self.inception2_mxp = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.inception2 = Inception(width=width, in_ch=128, out_ch=64)
+        self.inception3 = Inception(width=width, in_ch=64, out_ch=128)
+        self.inception3_mxp = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
 
-        self.inception3 = Inception(width=width, in_ch=32, out_ch=64)
-        self.inception4 = Inception(width=width, in_ch=64, out_ch=32)
+        self.inception4 = Inception(width=width, in_ch=128, out_ch=64)
+        self.inception5 = Inception(width=width, in_ch=64, out_ch=128)
+        self.inception5_mxp = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
 
-        self.linearWidth = 32 * 8 * 8
+        self.inception6 = Inception(width=width, in_ch=128, out_ch=64)
+
+        self.linearWidth = 64 * 4 * 4
         self.fc1_out = nn.Linear(self.linearWidth, 512)
         self.fc2_out = nn.Linear(512, 64)
         self.fc3_out = nn.Linear(64, num_class)
@@ -82,12 +86,16 @@ class InceptionThrNet(nn.Module):
         i1_out_dwn = self.inception1_mxp(i1_out)
 
         i2_out = self.inception2(torch.cat([i1_out[:, :, 8:24, 8:24], i1_out_dwn], 1))
-        i2_dwn_out = self.inception2_mxp(i2_out)
+        i3_out = self.inception3(i2_out)
+        i3_dwn_out = self.inception3_mxp(i3_out)
 
-        i3_out = self.inception3(i2_dwn_out)
-        i4_out = self.inception4(i3_out)
+        i4_out = self.inception4(i3_dwn_out)
+        i5_out = self.inception5(i4_out)
+        i5_dwn_out = self.inception5_mxp(i5_out)
 
-        flattened = i4_out.view(-1, self.linearWidth)
+        i6_out = self.inception6(i5_dwn_out)
+
+        flattened = i6_out.view(-1, self.linearWidth)
         # flattened = F.dropout2d(flattened, p=0.4)
         fc1_out = F.relu(self.fc1_out(flattened))
         fc2_out = F.relu(self.fc2_out(fc1_out))
