681496c60f8735693f3a5d737baafb46eb0f652e
Aashis Khanal <aturegsu@gmail.com>
2018-10-28 10:52:22 -0400

    Universal unet

--- neuralnet/unet/model.py
+++ neuralnet/unet/model.py
@@ -6,13 +6,13 @@ from neuralnet.utils.weights_utils import initialize_weights
 
 
 class _DoubleConvolution(nn.Module):
-    def __init__(self, in_channels, middle_channel, out_channels):
+    def __init__(self, in_channels, middle_channel, out_channels, p=0):
         super(_DoubleConvolution, self).__init__()
         layers = [
-            nn.Conv2d(in_channels, middle_channel, kernel_size=3),
+            nn.Conv2d(in_channels, middle_channel, kernel_size=3, padding=p),
             nn.BatchNorm2d(middle_channel),
             nn.ReLU(inplace=True),
-            nn.Conv2d(middle_channel, out_channels, kernel_size=3),
+            nn.Conv2d(middle_channel, out_channels, kernel_size=3, padding=p),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True)
         ]
@@ -25,53 +25,59 @@ class _DoubleConvolution(nn.Module):
 class UNet(nn.Module):
     def __init__(self, num_channels, num_classes):
         super(UNet, self).__init__()
-        mul = 16 if torch.cuda.is_available() else 1
-        self.enc1 = _DoubleConvolution(num_channels, 4 * mul, 4 * mul)
-        self.enc2 = _DoubleConvolution(4 * mul, 8 * mul, 8 * mul)
-        self.enc3 = _DoubleConvolution(8 * mul, 16 * mul, 16 * mul)
-        self.enc4 = _DoubleConvolution(16 * mul, 32 * mul, 32 * mul)
 
-        self.dec4 = _DoubleConvolution(32 * mul, 64 * mul, 64 * mul)
-        self.dec4_up = nn.ConvTranspose2d(64 * mul, 32 * mul, kernel_size=2, stride=2)
+        reduce_by = 1
 
-        self.dec3 = _DoubleConvolution(64 * mul, 32 * mul, 32 * mul)
-        self.dec3_up = nn.ConvTranspose2d(32 * mul, 16 * mul, kernel_size=2, stride=2)
+        self.A1_ = _DoubleConvolution(num_channels, int(64 / reduce_by), int(64 / reduce_by))
+        self.A2_ = _DoubleConvolution(int(64 / reduce_by), int(128 / reduce_by), int(128 / reduce_by))
+        self.A3_ = _DoubleConvolution(int(128 / reduce_by), int(256 / reduce_by), int(256 / reduce_by))
+        self.A4_ = _DoubleConvolution(int(256 / reduce_by), int(512 / reduce_by), int(512 / reduce_by))
 
-        self.dec2 = _DoubleConvolution(32 * mul, 16 * mul, 16 * mul)
-        self.dec2_up = nn.ConvTranspose2d(16 * mul, 8 * mul, kernel_size=2, stride=2)
+        self.A_mid = _DoubleConvolution(int(512 / reduce_by), int(1024 / reduce_by), int(1024 / reduce_by))
 
-        self.dec1 = _DoubleConvolution(16 * mul, 8 * mul, 8 * mul)
-        self.dec1_up = nn.ConvTranspose2d(8 * mul, 4 * mul, kernel_size=2, stride=2)
+        self.A4_up = nn.ConvTranspose2d(int(1024 / reduce_by), int(512 / reduce_by), kernel_size=2, stride=2)
+        self._A4 = _DoubleConvolution(int(1024 / reduce_by), int(512 / reduce_by), int(512 / reduce_by))
 
-        self.out = _DoubleConvolution(8 * mul, 4 * mul, 4 * mul)
-        self.final = nn.Conv2d(4 * mul, num_classes, kernel_size=1)
+        self.A3_up = nn.ConvTranspose2d(int(512 / reduce_by), int(256 / reduce_by), kernel_size=2, stride=2)
+        self._A3 = _DoubleConvolution(int(512 / reduce_by), int(256 / reduce_by), int(256 / reduce_by))
 
+        self.A2_up = nn.ConvTranspose2d(int(256 / reduce_by), int(128 / reduce_by), kernel_size=2, stride=2)
+        self._A2 = _DoubleConvolution(int(256 / reduce_by), int(128 / reduce_by), int(128 / reduce_by))
+
+        self.A1_up = nn.ConvTranspose2d(int(128 / reduce_by), int(64 / reduce_by), kernel_size=2, stride=2)
+        self._A1 = _DoubleConvolution(int(128 / reduce_by), int(64 / reduce_by), int(64 / reduce_by))
+
+        self.final = nn.Conv2d(int(64 / reduce_by), num_classes, kernel_size=1)
         initialize_weights(self)
 
     def forward(self, x):
-        enc1_ = self.enc1(x)
-        enc1 = F.max_pool2d(enc1_, kernel_size=2, stride=2)
+        a1_ = self.A1_(x)
+        a1_dwn = F.max_pool2d(a1_, kernel_size=2, stride=2)
 
-        enc2_ = self.enc2(enc1)
-        enc2 = F.max_pool2d(enc2_, kernel_size=2, stride=2)
+        a2_ = self.A2_(a1_dwn)
+        a2_dwn = F.max_pool2d(a2_, kernel_size=2, stride=2)
 
-        enc3_ = self.enc3(enc2)
-        enc3 = F.max_pool2d(enc3_, kernel_size=2, stride=2)
+        a3_ = self.A3_(a2_dwn)
+        a3_dwn = F.max_pool2d(a3_, kernel_size=2, stride=2)
 
-        enc4_ = self.enc4(enc3)
-        enc4 = F.max_pool2d(enc4_, kernel_size=2, stride=2)
-        enc4 = F.dropout2d(enc4, p=0.2)
+        a4_ = self.A4_(a3_dwn)
+        a4_dwn = F.max_pool2d(a4_, kernel_size=2, stride=2)
 
-        dec4 = self.dec4(enc4)
-        dec4 = F.dropout2d(dec4, p=0.5)
+        a_mid = self.A_mid(a4_dwn)
 
-        dec3 = self.dec3(UNet.match_and_concat(enc4_, self.dec4_up(dec4)))
-        dec3 = F.dropout2d(dec3, p=0.2)
+        a4_up = self.A4_up(a_mid)
+        _a4 = self._A4(UNet.match_and_concat(a4_, a4_up))
 
-        dec2 = self.dec2(UNet.match_and_concat(enc3_, self.dec3_up(dec3)))
-        dec1 = self.dec1(UNet.match_and_concat(enc2_, self.dec2_up(dec2)))
-        out = self.out(UNet.match_and_concat(enc1_, self.dec1_up(dec1)))
-        final = self.final(out)
+        a3_up = self.A3_up(_a4)
+        _a3 = self._A3(UNet.match_and_concat(a3_, a3_up))
+
+        a2_up = self.A2_up(_a3)
+        _a2 = self._A2(UNet.match_and_concat(a2_, a2_up))
+
+        a1_up = self.A1_up(_a2)
+        _a1 = self._A1(UNet.match_and_concat(a1_, a1_up))
+
+        final = self.final(_a1)
         return F.log_softmax(final, dim=1)
 
     @staticmethod
