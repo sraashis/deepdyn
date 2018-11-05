6035f917ea5512d4c9c0308068352d914a1342af
Aashis Khanal <aturegsu@gmail.com>
2018-11-05 00:51:30 -0500

    DICE LOSS WEIGHT ADDED.

--- neuralnet/utils/loss.py
+++ neuralnet/utils/loss.py
@@ -1,5 +1,6 @@
-def dice_loss(outputs, target, beta=1):
+def dice_loss(outputs, target, weights=1, beta=1):
     """
+    :param weights: element-wise weights
     :param outputs:
     :param target:
     :param beta: More beta, better precision. 1 is neutral
@@ -9,7 +10,7 @@ def dice_loss(outputs, target, beta=1):
 
     iflat = outputs.contiguous().float().view(-1)
     tflat = target.contiguous().float().view(-1)
-    intersection = (iflat * tflat).sum()
+    intersection = (iflat * tflat * weights).sum()
 
     f = (((1 + beta ** 2) * intersection) + smooth) / (((beta ** 2 * iflat.sum()) + tflat.sum()) + smooth)
     return 1 - f
