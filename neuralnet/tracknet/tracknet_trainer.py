e7877337e968c2c29311346c8b6477d6dc0531d3
saeidmotevali <stay4me>
2018-12-03 13:52:33 -0500

    4 layers working.

--- neuralnet/tracknet/tracknet_trainer.py
+++ neuralnet/tracknet/tracknet_trainer.py
@@ -84,13 +84,14 @@ class TracknetTrainer(NNTrainer):
                     positions = data['POS'].to(self.device)
                     outputs = self.model(inputs).squeeze()
                     predicted = outputs + positions.float()
-                    loss = F.mse_loss(outputs.squeeze(), labels.squeeze())
 
+                    loss = F.mse_loss(outputs.squeeze(), labels.squeeze())
                     current_loss = math.sqrt(loss.item())
                     img_loss += current_loss
-
                     if len(outputs.shape) == 1:
                         outputs = outputs[None, ...]
+
+                    labels = labels + positions.float()
                     for j in range(outputs.shape[0]):
                         x, y = int(labels[j][0]), int(labels[j][1])
                         x_pred, y_pred = int(predicted[j][0]), int(predicted[j][1])
