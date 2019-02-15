6034cfcf7f8b36801c22a6b20f1ce08846c02891
aashis <g2aashis@gmail.com>
2019-02-15 12:31:18 -0500

    Setup.py for local installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ature',
+      version='0.1',
+      description='Moduel for Research',
+      url='http://github.com/sraashis/ature',
+      author='Aashis Khanal',
+      author_email='sraahis@gmail.com',
+      license='MIT',
+      packages=['commons', 'neuralnet', 'utils'],
+      zip_safe=False)
