f9d3f70a4329e15dfc66ba018b174a437f28a8a5
saeidmotevali <stay4me>
2019-02-25 13:32:30 -0500

    4 layers working. current point + previous point(Original image(green chanel) and segmented image). cosine error. One outputs used polar system. Phi is the only output.

--- neuralnet/tracknet/tracknet_dataloader.py
+++ neuralnet/tracknet/tracknet_dataloader.py
@@ -26,8 +26,6 @@ class PatchesGenerator(Generator):
         for ID, img_file in enumerate(self.images):
             mat_file = Mat(mat_file=self.image_dir + sep + img_file)
 
-
-
             V = mat_file.get_graph('V')
             A = mat_file.get_graph('A')
             I = mat_file.get_image('I')
@@ -54,6 +52,8 @@ class PatchesGenerator(Generator):
                 b[i] = np.where(A[src, :])[0][0]
             b_pos_output = V[b, :]
             u_pos_input = u_pos_input.astype(np.int)
+            maxoutput = 0.0
+            maxout = 0.0
 
             temp = img_obj.working_arr.copy()
             for (q, p), (j, i), output, b_out in zip(u_pos_input_prev, u_pos_input, b_pos_output - u_pos_input, b_pos_output):
@@ -76,18 +76,39 @@ class PatchesGenerator(Generator):
                 if np.isin(0, img_obj.mask[row_from:row_to, col_from:col_to]):
                     continue
 
+                # print('output before', output)
+                # your_permutation = [1, 0]
+                # order = np.argsort(your_permutation)
+                # output[:, order]
+                # print('ooouutt', output)
+
                 rho = np.sqrt(output[0] ** 2 + output[1] ** 2)
                 phi = np.arctan2(output[1], output[0])
                 # print(i, j, b_out, phi)
 
                 if phi < 0:
                     phi = (2 * math.pi) + phi
+                    # phi = math.pi + phi
+
                 phi = phi * 180 / math.pi
+
+                # if phi > (math.pi / 2):
+                #         phi = math.pi - phi
+                # print('ii,jj, b_out', i, j,  b_out)
 
                 # if phi == 0:
                 #     img_obj = 255
-                self.indices.append([ID, [p, q], [i, j], phi, b_out])
+                # if maxout < abs(output[0]):
+                #     maxout = abs(output[0])
+                # if maxoutput < abs(output[1]):
+                #     maxoutput = abs(output[1])
+                # s = s[::-1]
+                b_out = b_out[::-1]
+                # print('i, j, output', i, j, p, q, type(b_out), b_out)
+                # print('phi', phi)
+                self.indices.append([ID, [p, q], [i, j], phi, output])
             IMG.fromarray(temp).save('patches/'+img_file+'.png')
+            # print('maxxiiimuum', maxout, maxoutput)
 
     def __getitem__(self, index):
         ID, (p, q), (i, j), out, b_pos = self.indices[index]
@@ -111,8 +132,8 @@ class PatchesGenerator(Generator):
         input_patches = self.image_objects[ID].res['2d'][:, row_from:row_to, col_from:col_to]
 
         input_tensor = np.append(prev_patches, input_patches, 0)
-        # if out == 0:
-            # print('hhhss', out, i, j, b_pos)
+        # if out > 350:
+        #     print('hhhss', out, i, j, b_pos)
             # IMG.fromarray(img_obj)
             # IMG.fromarray(input_patches[1, :, :]).save('patches/'+str(index) + '.png')
         return {'IDs': ID, 'POS': np.array([i, j]), 'PREV': np.array([p, q]), 'inputs': input_tensor,
