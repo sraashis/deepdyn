b3f9f42eeb982a7463362d651d660df350e30bc4
akhanal1 <sraashis@gmail.com>
2019-03-14 04:21:05 -0400

    New approach

--- neuralnet/unet/main.py
+++ neuralnet/unet/main.py
@@ -28,7 +28,7 @@ from neuralnet.utils.measurements import ScoreAccumulator
 #          rs.WIDE, rs.WIDE3,
 #          rs.VEVIO, rs.VEVIO3]
 
-RUNS = [rs.VEVIO, rs.VEVIO1, rs.VEVIO2]
+RUNS = [rs.DRIVE]
 
 
 def main():
@@ -37,7 +37,7 @@ def main():
         transforms.ToTensor()
     ])
 
-    for R in [rs.VEVIO]:
+    for R in RUNS:
         for k, folder in R['Dirs'].items():
             os.makedirs(folder, exist_ok=True)
 
@@ -54,25 +54,20 @@ def main():
                 optimizer = optim.Adam(model.module.parameters(), lr=R['Params']['learning_rate'])
 
             try:
-                drive_trainer = UNetNNTrainer(model=model, run_conf=R)
+                drive_trainer = UNetNNTrainer(model=model, run_conf=R, optimizer=optimizer)
                 if R.get('Params').get('mode') == 'train':
                     train_loader = PatchesGenerator.get_loader(run_conf=R, images=splits['train'], transforms=transform,
                                                                mode='train')
                     val_loader = PatchesGenerator.get_loader_per_img(run_conf=R, images=splits['validation'],
                                                                      mode='validation')
-                    drive_trainer.train(optimizer=optimizer, data_loader=train_loader, validation_loader=val_loader)
+                    drive_trainer.train(data_loader=train_loader, validation_loader=val_loader,
+                                        epoch_run=drive_trainer.epoch_ce_loss)
 
                 drive_trainer.resume_from_checkpoint(parallel_trained=R.get('Params').get('parallel_trained'))
 
-                images = splits['test']
                 test_loader = PatchesGenerator.get_loader_per_img(run_conf=R,
-                                                                  images=images, mode='test')
-
-                logger = drive_trainer.get_logger(drive_trainer.test_log_file,
-                                                  header='ID,PRECISION,RECALL,F1,ACCURACY')
-                drive_trainer.evaluate(data_loaders=test_loader, logger=logger, gen_images=True)
-                drive_trainer.plot_test(file=drive_trainer.test_log_file)
-                logger.close()
+                                                                  images=splits['test'], mode='test')
+                drive_trainer.test(test_loader)
             except Exception as e:
                 traceback.print_exc()
 
