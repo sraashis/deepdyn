7c2e24986cc54c7fbd1d486f97d3a0fffeb856e5
sraashis <sraashis@gmail.com>
2019-11-05 09:38:33 -0500

    All dataset in main.

--- main.py
+++ main.py
@@ -12,20 +12,20 @@ transforms = tmf.Compose([
 
 if __name__ == "__main__":
     runs_unet = [
-        ru.STARE_1_100_1, ru.STARE_WEIGHTED,
-        ru.WIDE_1_100_1, ru.WIDE_WEIGHTED,
-        ru.CHASEDB_1_100_1, ru.CHASEDB_WEIGHTED,
-        ru.VEVIO_MOSAICS_1_100_1, ru.VEVIO_MOSAICS_WEIGHTED,
-        ru.VEVIO_FRAMES_1_100_1, ru.VEVIO_FRAMES_WEIGHTED,
-        ru.DRIVE_1_100_1, ru.DRIVE_WEIGHTED]
+        ru.STARE_1_100_1, ru.STARE_1_1, ru.STARE_WEIGHTED,
+        ru.WIDE_1_100_1, ru.WIDE_1_1, ru.WIDE_WEIGHTED,
+        ru.CHASEDB_1_100_1, ru.CHASEDB_1_1, ru.CHASEDB_WEIGHTED,
+        ru.VEVIO_MOSAICS_1_100_1, ru.VEVIO_MOSAICS_1_1, ru.VEVIO_MOSAICS_WEIGHTED,
+        ru.VEVIO_FRAMES_1_100_1, ru.VEVIO_FRAMES_1_1, ru.VEVIO_FRAMES_WEIGHTED,
+        ru.DRIVE_1_100_1, ru.DRIVE_1_1, ru.DRIVE_WEIGHTED]
 
     runs_miniunet = [
-        rm.STARE_1_100_1, rm.STARE_WEIGHTED,
-        rm.WIDE_1_100_1, rm.WIDE_WEIGHTED,
-        rm.CHASEDB_1_100_1, rm.CHASEDB_WEIGHTED,
-        rm.VEVIO_MOSAICS_1_100_1, rm.VEVIO_MOSAICS_WEIGHTED,
-        rm.VEVIO_FRAMES_1_100_1, rm.VEVIO_FRAMES_WEIGHTED,
-        rm.DRIVE_1_100_1, rm.DRIVE_WEIGHTED]
+        rm.STARE_1_100_1, rm.STARE_1_1, rm.STARE_WEIGHTED,
+        rm.WIDE_1_100_1, rm.WIDE_1_1, rm.WIDE_WEIGHTED,
+        rm.CHASEDB_1_100_1, rm.CHASEDB_1_1, rm.CHASEDB_WEIGHTED,
+        rm.VEVIO_MOSAICS_1_100_1, rm.VEVIO_MOSAICS_1_1, rm.VEVIO_MOSAICS_WEIGHTED,
+        rm.VEVIO_FRAMES_1_100_1, rm.VEVIO_FRAMES_1_1, rm.VEVIO_FRAMES_WEIGHTED,
+        rm.DRIVE_1_100_1, rm.DRIVE_1_1, rm.DRIVE_WEIGHTED]
 
     for r_unet, r_miniunet in zip(runs_unet, runs_miniunet):
         unet.run([r_unet], transforms)
